0578781775fe4a4b605a620b86f5ccb790700f10
httpdigest <kburjack@googlemail.com>
2015-10-02 21:56:31 +0200

    Use complementary colors and thicker lines

--- res/org/lwjgl/demo/opengl/geometry/fs.glsl
+++ res/org/lwjgl/demo/opengl/geometry/fs.glsl
@@ -1,7 +1,7 @@
 #version 150
 
 const vec3 edgeColor = vec3(0.0, 0.0, 0.0);
-const vec3 faceColor = vec3(0.2, 0.5, 0.8);
+const vec3 faceColor = vec3(0.95, 0.7, 0.1);
 
 in VertexData {
   noperspective vec3 distance;
@@ -12,7 +12,7 @@ out vec4 color;
 void main(void) {
   // determine frag distance to closest edge
   float fNearest = min(min(vVertexIn.distance[0], vVertexIn.distance[1]), vVertexIn.distance[2]);
-  float fEdgeIntensity = clamp(exp2(-0.2 * fNearest * fNearest), 0.0, 1.0);
+  float fEdgeIntensity = clamp(exp2(-0.1 * fNearest * fNearest), 0.0, 1.0);
 
   // blend between edge color and face color
   color = vec4(mix(faceColor, edgeColor, fEdgeIntensity), 1.0);
