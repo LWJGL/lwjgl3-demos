6144c08e33311b7ae3b62fdbbb9afa510a8ef2f7
Kai Burjack <kburjack@googlemail.com>
2015-10-19 13:05:31 +0200

    Linear filtering of DEPTH or RGBA32F is not supported in most cards

--- src/org/lwjgl/demo/opengl/fbo/DepthEdgeShaderDemo20.java
+++ src/org/lwjgl/demo/opengl/fbo/DepthEdgeShaderDemo20.java
@@ -177,14 +177,14 @@ public class DepthEdgeShaderDemo20 {
         }
         normalTexture = glGenTextures();
         glBindTexture(GL_TEXTURE_2D, normalTexture);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F_ARB, width, height, 0, GL_RGBA, GL_FLOAT, 0L);
         glBindTexture(GL_TEXTURE_2D, 0);
         depthTexture = glGenTextures();
         glBindTexture(GL_TEXTURE_2D, depthTexture);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, width, height, 0, GL_DEPTH_COMPONENT, GL_UNSIGNED_BYTE, 0L);
         glBindTexture(GL_TEXTURE_2D, 0);
     }
