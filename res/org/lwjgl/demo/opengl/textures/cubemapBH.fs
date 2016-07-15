69418956fd00b7c790170a9024a183b325677b22
httpdigest <kburjack@googlemail.com>
2016-07-15 23:01:09 +0200

    Add redshift of light near the black hole

--- res/org/lwjgl/demo/opengl/textures/cubemapBH.fs
+++ res/org/lwjgl/demo/opengl/textures/cubemapBH.fs
@@ -14,8 +14,8 @@ uniform float debug;
 varying vec3 dir;
 
 // Could probably be uniforms
-#define blackholeStrength 4.0
-#define blackholeHorizonSharpness 1
+#define blackholeStrength 6.0
+#define blackholeHorizonSharpness 2
 
 float interpolate(float edge0, float edge1, float x) {
   int i;
@@ -30,6 +30,36 @@ vec3 planeIntersect() {
   vec3 planeNormal = cameraPosition - blackholePosition;
   float t = -dot(planeNormal, planeNormal) / dot(planeNormal, dir);
   return cameraPosition + dir * t;
+}
+
+/**
+ * http://lolengine.net/blog/2013/07/27/rgb-to-hsv-in-glsl
+ */
+vec3 rgb2hsv(vec3 c) {
+  vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+  vec4 p = c.g < c.b ? vec4(c.bg, K.wz) : vec4(c.gb, K.xy);
+  vec4 q = c.r < p.x ? vec4(p.xyw, c.r) : vec4(c.r, p.yzx);
+  float d = q.x - min(q.w, q.y);
+  float e = 1.0e-10;
+  return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+}
+
+/**
+ * http://lolengine.net/blog/2013/07/27/rgb-to-hsv-in-glsl
+ */
+vec3 hsv2rgb(vec3 c) {
+  vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+  vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+  return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+/**
+ * Add some fake redshift to the light based on the distance to the black hole.
+ */
+vec4 redshift(vec4 col, float w) {
+  vec3 hsv = rgb2hsv(col.rgb);
+  hsv.r *= w*w*w*w; // <- shift towards red (which is hue = 0).
+  return vec4(hsv2rgb(hsv), 1.0);
 }
 
 vec4 distortion(void) {
@@ -61,5 +91,5 @@ void main(void) {
     color = vec4(0.9, 1.7, 0.9, 1.0);
   }
   vec4 dist = distortion();
-  gl_FragColor = color * textureCube(tex, dist.xyz) * dist.w;
+  gl_FragColor = color * redshift(textureCube(tex, dist.xyz), dist.w) * dist.w;
 }
