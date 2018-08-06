66cfd34c3ff6c27c340003cbeee6a1932b4a392f
httpdigest <kburjack@googlemail.com>
2018-08-06 10:35:37 +0200

    Fix loading models from classpath using aiImportFileEx() (#19)

--- src/org/lwjgl/demo/opengl/assimp/WavefrontObjDemo.java
+++ src/org/lwjgl/demo/opengl/assimp/WavefrontObjDemo.java
@@ -11,7 +11,6 @@ import org.lwjgl.opengl.GL;
 import org.lwjgl.opengl.GLCapabilities;
 import org.lwjgl.opengl.GLUtil;
 import org.lwjgl.system.*;
-import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
@@ -154,7 +153,6 @@ public class WavefrontObjDemo {
         glfwSetWindowPos(window, (vidmode.width() - width) / 2, (vidmode.height() - height) / 2);
         glfwMakeContextCurrent(window);
         glfwSwapInterval(0);
-        glfwShowWindow(window);
         glfwSetCursorPos(window, width / 2, height / 2);
 
         try (MemoryStack frame = MemoryStack.stackPush()) {
@@ -182,14 +180,61 @@ public class WavefrontObjDemo {
         /* Create all needed GL resources */
         loadModel();
         createProgram();
+
+        /* Show window */
+        glfwShowWindow(window);
     }
 
     void loadModel() {
-        String fileName = Thread.currentThread().getContextClassLoader()
-                .getResource("org/lwjgl/demo/opengl/assimp/magnet.obj").getFile();
-        File file = new File(fileName);
-        // Assimp will be able to find the corresponding mtl file if we call aiImportFile this way.
-        AIScene scene = aiImportFile(file.getAbsolutePath(), aiProcess_JoinIdenticalVertices | aiProcess_Triangulate);
+        AIFileIO fileIo = AIFileIO.create();
+        AIFileOpenProcI fileOpenProc = new AIFileOpenProc() {
+            public long invoke(long pFileIO, long fileName, long openMode) {
+                AIFile aiFile = AIFile.create();
+                final ByteBuffer data;
+                String fileNameUtf8 = memUTF8(fileName);
+                try {
+                    data = ioResourceToByteBuffer(fileNameUtf8, 8192);
+                } catch (IOException e) {
+                    throw new RuntimeException("Could not open file: " + fileNameUtf8);
+                }
+                AIFileReadProcI fileReadProc = new AIFileReadProc() {
+                    public long invoke(long pFile, long pBuffer, long size, long count) {
+                        long max = Math.min(data.remaining(), size * count);
+                        memCopy(memAddress(data) + data.position(), pBuffer, max);
+                        return max;
+                    }
+                };
+                AIFileSeekI fileSeekProc = new AIFileSeek() {
+                    public int invoke(long pFile, long offset, int origin) {
+                        if (origin == Assimp.aiOrigin_CUR) {
+                            data.position(data.position() + (int) offset);
+                        } else if (origin == Assimp.aiOrigin_SET) {
+                            data.position((int) offset);
+                        } else if (origin == Assimp.aiOrigin_END) {
+                            data.position(data.limit() + (int) offset);
+                        }
+                        return 0;
+                    }
+                };
+                AIFileTellProcI fileTellProc = new AIFileTellProc() {
+                    public long invoke(long pFile) {
+                        return data.capacity();
+                    }
+                };
+                aiFile.ReadProc(fileReadProc);
+                aiFile.SeekProc(fileSeekProc);
+                aiFile.FileSizeProc(fileTellProc);
+                return aiFile.address();
+            }
+        };
+        AIFileCloseProcI fileCloseProc = new AIFileCloseProc() {
+            public void invoke(long pFileIO, long pFile) {
+                /* Nothing to do */
+            }
+        };
+        fileIo.set(fileOpenProc, fileCloseProc, NULL);
+        AIScene scene = aiImportFileEx("org/lwjgl/demo/opengl/assimp/magnet.obj",
+                aiProcess_JoinIdenticalVertices | aiProcess_Triangulate, fileIo);
         if (scene == null) {
             throw new IllegalStateException(aiGetErrorString());
         }
