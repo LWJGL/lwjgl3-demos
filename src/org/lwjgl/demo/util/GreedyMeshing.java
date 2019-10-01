7d27552c2f939f93f74dd56bb5b99b4b63669fec
httpdigest <kburjack@googlemail.com>
2019-10-01 19:52:29 +0200

    Refactor GreedyMesh

--- src/org/lwjgl/demo/util/GreedyMeshing.java
+++ src/org/lwjgl/demo/util/GreedyMeshing.java
@@ -1,15 +1,19 @@
 package org.lwjgl.demo.util;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * Greedy meshing based on the JavaScript code from
  * https://0fps.net/2012/07/07/meshing-minecraft-part-2/
  * 
+ * Instances of this class are <i>not</i> thread-safe, so calls to
+ * {@link #mesh(byte[], List)} on the same instance must be externally
+ * synchronized.
+ * 
  * @author Kai Burjack
  */
 public class GreedyMeshing {
-
     public static class Face {
         public byte u0, v0, u1, v1, p, s;
 
@@ -23,36 +27,46 @@ public class GreedyMeshing {
         }
     }
 
-    private static short at(byte[] vs, int[] dims, int x, int y, int z) {
-        return vs[x + dims[0] * (y + dims[1] * z)];
+    private final int[] du = new int[3], dv = new int[3], q = new int[3], x = new int[3];
+    private final int[][] m;
+    private final int dx, dy;
+    private final int[] dims;
+
+    public GreedyMeshing(int dx, int dy, int dz) {
+        this.dx = dx;
+        this.dy = dy;
+        this.m = new int[][] { new int[dy * dz], new int[dz * dx], new int[dx * dy] };
+        this.dims = new int[] { dx, dy, dz };
     }
 
-    public static void mesh(byte[] vs, int[] dims, List<Face> faces) {
-        int[] du = new int[3], dv = new int[3], q = new int[3], x = new int[3];
+    private short at(byte[] vs, int x, int y, int z) {
+        return vs[x + dx * (y + dy * z)];
+    }
+
+    public void mesh(byte[] vs, List<Face> faces) {
         for (int d = 0; d < 3; d++) {
             int u = (d + 1) % 3, v = (d + 2) % 3;
-            int[] m = new int[dims[u] * dims[v]];
-            for (int i = 0; i < 3; i++)
-                q[i] = 0;
+            int[] m = this.m[d];
+            Arrays.fill(q, 0);
             q[d] = 1;
             for (x[d] = -1; x[d] < dims[d];) {
-                generateMask(vs, dims, q, x, d, u, v, m);
+                generateMask(vs, d, u, v, m);
                 x[d]++;
-                mergeAndGenerateFaces(dims, faces, du, dv, x, u, v, m, d);
+                mergeAndGenerateFaces(faces, u, v, m, d);
             }
         }
     }
 
-    private static void generateMask(byte[] vs, int[] dims, int[] q, int[] x, int d, int u, int v, int[] m) {
+    private void generateMask(byte[] vs, int d, int u, int v, int[] m) {
         int n = 0;
         for (x[v] = 0; x[v] < dims[v]; x[v]++)
             for (x[u] = 0; x[u] < dims[u]; x[u]++, n++)
-                generateMask(vs, dims, q, x, d, m, n);
+                generateMask(vs, d, m, n);
     }
 
-    private static void generateMask(byte[] vs, int[] dims, int[] q, int[] x, int d, int[] m, int n) {
-        int a = x[d] >= 0 ? at(vs, dims, x[0], x[1], x[2]) : 0;
-        int b = x[d] < dims[d] - 1 ? at(vs, dims, x[0] + q[0], x[1] + q[1], x[2] + q[2]) : 0;
+    private void generateMask(byte[] vs, int d, int[] m, int n) {
+        int a = x[d] >= 0 ? at(vs, x[0], x[1], x[2]) : 0;
+        int b = x[d] < dims[d] - 1 ? at(vs, x[0] + q[0], x[1] + q[1], x[2] + q[2]) : 0;
         if (a == b)
             m[n] = 0;
         else if (a != 0)
@@ -61,33 +75,30 @@ public class GreedyMeshing {
             m[n] = -b;
     }
 
-    private static void mergeAndGenerateFaces(int[] dims, List<Face> faces, int[] du, int[] dv, int[] x, int u, int v,
-            int[] m, int d) {
+    private void mergeAndGenerateFaces(List<Face> faces, int u, int v, int[] m, int d) {
         for (int j = 0, n = 0; j < dims[v]; ++j)
             for (int i = 0, incr; i < dims[u]; i += incr, n += incr)
-                incr = mergeAndGenerateFace(dims, faces, du, dv, x, u, v, m, n, j, i, d);
+                incr = mergeAndGenerateFace(faces, u, v, m, n, j, i, d);
     }
 
-    private static int mergeAndGenerateFace(int[] dims, List<Face> faces, int[] du, int[] dv, int[] x, int u, int v,
-            int[] m, int n, int j, int i, int d) {
+    private int mergeAndGenerateFace(List<Face> faces, int u, int v, int[] m, int n, int j, int i, int d) {
         if (m[n] == 0)
             return 1;
-        int w = determineWidth(dims, u, m, n, i, m[n]);
-        int h = determineHeight(dims, u, v, m, n, j, m[n], w);
-        int s = determineFaceRegion(du, dv, x, u, v, m, n, j, i, d, w, h);
-        addFace(faces, du, dv, x, u, v, d, s);
-        eraseMask(dims, u, m, n, w, h);
+        int w = determineWidth(u, m, n, i, m[n]);
+        int h = determineHeight(u, v, m, n, j, m[n], w);
+        int s = determineFaceRegion(u, v, m, n, j, i, d, w, h);
+        addFace(faces, u, v, d, s);
+        eraseMask(u, m, n, w, h);
         return w;
     }
 
-    private static void eraseMask(int[] dims, int u, int[] m, int n, int w, int h) {
+    private void eraseMask(int u, int[] m, int n, int w, int h) {
         for (int l = 0; l < h; l++)
             for (int k = 0; k < w; k++)
                 m[n + k + l * dims[u]] = 0;
     }
 
-    private static int determineFaceRegion(int[] du, int[] dv, int[] x, int u, int v, int[] m, int n, int j, int i,
-            int d, int w, int h) {
+    private int determineFaceRegion(int u, int v, int[] m, int n, int j, int i, int d, int w, int h) {
         x[u] = i;
         x[v] = j;
         if (m[n] > 0) {
@@ -103,14 +114,14 @@ public class GreedyMeshing {
         }
     }
 
-    private static int determineWidth(int[] dims, int u, int[] m, int n, int i, int c) {
+    private int determineWidth(int u, int[] m, int n, int i, int c) {
         int w = 1;
         while (n + w < m.length && i + w < dims[u] && c == m[n + w])
             w++;
         return w;
     }
 
-    private static int determineHeight(int[] dims, int u, int v, int[] m, int n, int j, int c, int w) {
+    private int determineHeight(int u, int v, int[] m, int n, int j, int c, int w) {
         int h;
         boolean done = false;
         for (h = 1; j + h < dims[v]; h++) {
@@ -125,8 +136,7 @@ public class GreedyMeshing {
         return h;
     }
 
-    private static void addFace(List<Face> faces, int[] du, int[] dv, int[] x, int u, int v, int d, int s) {
+    private void addFace(List<Face> faces, int u, int v, int d, int s) {
         faces.add(new Face(x[u], x[v], x[u] + du[u] + dv[u], x[v] + du[v] + dv[v], x[d], (d << 1) + s));
     }
-
 }
