4647b2589a5221b7e3ae9c622917a3323e435cd7
httpdigest <kburjack@googlemail.com>
2019-10-10 22:27:18 +0200

    Add demo that generates blue-noise random samples via [1]
    [1]: https://eheitzresearch.wordpress.com/762-2/

---
+++ res/org/lwjgl/demo/opengl/raytracing/tutorial4_3/random.glsl
@@ -0,0 +1,287 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: https://www.lwjgl.org/license
+ */
+#version 330 core
+
+#define PI 3.14159265359
+#define TWO_PI 6.28318530718
+#define ONE_OVER_PI (1.0 / PI)
+#define ONE_OVER_2PI (1.0 / TWO_PI)
+
+/*
+ * Define the type of a random variable/vector which is used to compute
+ * spatial values (positions, directions, angles, etc.).
+ * Actually this should have been a typedef, but we can't do this in
+ * GLSL.
+ */
+#define spatialrand vec2
+
+/**
+ * Compute an arbitrary unit vector orthogonal to any vector 'v'.
+ *
+ * http://lolengine.net/blog/2013/09/21/picking-orthogonal-vector-combing-coconuts
+ *
+ * @param v the vector to compute an orthogonal unit vector from
+ * @returns the unit vector orthogonal to 'v'
+ */
+vec3 ortho(vec3 v) {
+  return normalize(mix(vec3(-v.y, v.x, 0.0), vec3(0.0, -v.z, v.y), abs(v.x) < abs(v.z)));
+}
+
+/**
+ * http://amindforeverprogramming.blogspot.de/2013/07/random-floats-in-glsl-330.html?showComment=1507064059398#c5427444543794991219
+ */
+uint hash2(uint x, uint y) {
+  x += x >> 11;
+  x ^= x << 7;
+  x += y;
+  x ^= x << 6;
+  x += x >> 15;
+  x ^= x << 5;
+  x += x >> 12;
+  x ^= x << 9;
+  return x;
+}
+
+/**
+ * http://amindforeverprogramming.blogspot.de/2013/07/random-floats-in-glsl-330.html?showComment=1507064059398#c5427444543794991219
+ */
+uint hash3(uint x, uint y, uint z) {
+  x += x >> 11;
+  x ^= x << 7;
+  x += y;
+  x ^= x << 3;
+  x += z ^ (x >> 14);
+  x ^= x << 6;
+  x += x >> 15;
+  x ^= x << 5;
+  x += x >> 12;
+  x ^= x << 9;
+  return x;
+}
+
+/**
+ * Generate a floating-point pseudo-random number in [0, 1).
+ *
+ * With Monte Carlo sampling we need random numbers to generate random
+ * vectors for shooting rays.
+ * This function takes a vector of three arbitrary floating-point
+ * numbers and based on those numbers returns a pseudo-random number in
+ * the range [0..1).
+ * Since in GLSL we have no other state/entropy than what we input as
+ * uniforms or compute from varying variables (e.g. the invocation id of
+ * the current work item), any "pseudo-random" number will necessarily
+ * only be some kind of hash over the input.
+ * This function however has very very good properties exhibiting no
+ * patterns in the distribution of the random numbers, no matter the
+ * magnitude of the input values.
+ * 
+ * http://amindforeverprogramming.blogspot.de/2013/07/random-floats-in-glsl-330.html
+ *
+ * @param f some input vector of numbers to generate a single
+ *          pseudo-random number from
+ * @returns a single pseudo-random number in [0, 1)
+ */
+float random3(vec3 f) {
+  uint mantissaMask = 0x007FFFFFu;
+  uint one = 0x3F800000u;
+  uvec3 u = floatBitsToUint(f);
+  uint h = hash3(u.x, u.y, u.z);
+  return uintBitsToFloat((h & mantissaMask) | one) - 1.0;
+}
+float random2(vec2 f) {
+  uint mantissaMask = 0x007FFFFFu;
+  uint one = 0x3F800000u;
+  uvec2 u = floatBitsToUint(f);
+  uint h = hash2(u.x, u.y);
+  return uintBitsToFloat((h & mantissaMask) | one) - 1.0;
+}
+
+/**
+ * Transform the given vector 'v' from its local frame into the
+ * orthonormal basis with +Z = z.
+ *
+ * @param v the vector to transform
+ * @param z the direction to transform +Z into
+ * @returns the vector in the new basis
+ */
+vec3 around(vec3 v, vec3 z) {
+  vec3 t = ortho(z), b = cross(z, t);
+  return t * v.x + b * v.y + z * v.z;
+}
+
+/**
+ * Compute the cartesian coordinates from the values typically computed
+ * when generating sample directions/angles for isotropic BRDFs.
+ *
+ * @param rp this is a pseudo-random number in [0, 1) representing the
+ *           angle `phi` to rotate around the principal vector. For
+ *           isotropic BRDFs where `phi` has the same probability of
+ *           being chosen, this will always be a random number in [0, 1)
+ *           that can directly be used from rand.x given to all sample
+ *           direction generation functions
+ * @param c this is the cosine of theta, the angle in [0, PI/2) giving
+ *          the angle between the principal vector and the one to
+ *          generate. Being the cosine of an angle in [0, PI/2) the
+ *          value 'c' is itself in the range [0, 1)
+ * @returns the cartesian direction vector
+ */
+vec3 isotropic(float rp, float c) {
+  float p = TWO_PI * rp, s = sqrt(1.0 - c*c);
+  return vec3(cos(p) * s, sin(p) * s, c);
+}
+
+/**
+ * Generate a uniformly distributed random vector on the hemisphere
+ * around the given normal vector 'n'.
+ *
+ * http://mathworld.wolfram.com/SpherePointPicking.html
+ *
+ * @param n the normal vector determining the direction of the
+ *          hemisphere
+ * @param rand a vector of two floating-point pseudo-random numbers
+ * @returns the random hemisphere vector plus its probability density
+ *          value
+ */
+vec4 randomHemisphereDirection(vec3 n, spatialrand rand) {
+  return vec4(around(isotropic(rand.x, rand.y), n), ONE_OVER_2PI);
+}
+/**
+ * Compute the probability density value of randomHemisphereDirection()
+ * generating the vector 'v'.
+ *
+ * @param n the normal vector determining the direction of the
+ *          hemisphere
+ * @param v the vector to compute the pdf of
+ * @returns pdf(v) for the uniform hemisphere distribution
+ */
+float randomHemisphereDirectionPDF(vec3 n, vec3 v) {
+  return step(0.0, dot(v, n)) * ONE_OVER_2PI;
+}
+
+/**
+ * Generate a cosine-weighted random vector on the hemisphere around the given
+ * normal vector 'n'.
+ *
+ * The probability density of any vector is directly proportional to the cosine
+ * of the angle between that vector and the given normal 'n'.
+ *
+ * http://www.rorydriscoll.com/2009/01/07/better-sampling/
+ *
+ * @param n the normal vector determining the direction of the hemisphere
+ * @param rand a vector of two floating-point pseudo-random numbers
+ * @returns the cosine-weighted random hemisphere vector plus its probability
+ *          density value
+ */
+vec4 randomCosineWeightedHemisphereDirection(vec3 n, spatialrand rand) {
+  float c = sqrt(rand.y);
+  return vec4(around(isotropic(rand.x, c), n), c * ONE_OVER_PI);
+}
+/**
+ * Evaluate the probability density function of the cosine-weighted distribution
+ * for the given sample vector 'v'.
+ *
+ * This tells the relative probability of randomCosineWeightedHemisphereDirection()
+ * generating that vector.
+ *
+ * @param r the direction of perfect reflection
+ * @param v the sample vector to evaluate the PDF for
+ * @returns pdf(v)
+ */
+float randomCosineWeightedHemisphereDirectionPDF(vec3 n, vec3 v) {
+  return max(0.0, dot(n, v)) * ONE_OVER_PI;
+}
+
+/**
+ * Generate a Phong-weighted random vector around the given reflection vector
+ * 'r'.
+ * Since the Phong BRDF has higher values when the outgoing vector is close to
+ * the perfect reflection vector of the incoming vector across the normal, we
+ * generate directions primarily around that reflection vector.
+ *
+ * http://blog.tobias-franke.eu/2014/03/30/notes_on_importance_sampling.html
+ *
+ * @param r the direction of perfect reflection
+ * @param a the power to raise the cosine term to in the Phong model
+ * @param rand a vector of two pseudo-random numbers
+ * @returns the Phong-weighted random vector
+ */
+vec4 randomPhongWeightedHemisphereDirection(vec3 r, float a, spatialrand rand) {
+  float ai = 1.0 / (a + 1.0), pr = (a + 1.0) * pow(rand.y, a * ai) * ONE_OVER_2PI;
+  return vec4(around(isotropic(rand.x, pow(rand.y, ai)), r), pr);
+}
+/**
+ * Evaluate the probability density function of the Phong-weighted distribution
+ * for the given sample vector 'v'.
+ *
+ * This tells the relative probability of randomPhongWeightedHemisphereDirection()
+ * generating that vector.
+ *
+ * @param r the direction of perfect reflection
+ * @param a the power to raise the cosine term to in the Phong model
+ * @param v the sample vector to evaluate the PDF for
+ * @returns pdf(v)
+ */
+float randomPhongWeightedHemisphereDirectionPDF(vec3 r, float a, vec3 v) {
+  return (a + 1.0) * pow(max(0.0, dot(r, v)), a) * ONE_OVER_2PI;
+}
+
+/**
+ * Generate a random vector uniformly distributed over the area of the rectangle
+ * with corner point 'c' and extents 'x' and 'y'.
+ *
+ * @param p the origin/point to sample from
+ * @param c the corner of the rectangle
+ * @param x the local X axis of the rectangle
+ * @param y the local Y axis of the rectangle
+ * @param rand a vector of two pseudo-random numbers
+ * @returns the random sample vector
+ */
+vec4 randomRectangleAreaDirection(vec3 p, vec3 c, vec3 x, vec3 y, spatialrand rand) {
+  vec3 s = c + rand.x * x + rand.y * y, sp = p - s;
+  float len2 = dot(sp, sp);
+  vec3 d = sp * inversesqrt(len2);
+  return vec4(-d, len2 / max(0.0, dot(d, cross(x, y))));
+}
+
+/**
+ * Determine whether the point 'p' lies inside of the rectangle
+ * with corner 'c' and spanning vectors 'x' and 'y'.
+ *
+ * More precisely, this function determines whether the projection
+ * of the point 'p' onto the plane with point 'c' and spanning vectors
+ * 'x' and 'y' is within the bounds of the rectangle defined by the
+ * corner point 'c' and the directions and lengths of 'x' and 'y'.
+ *
+ * @param p the point to test
+ * @param c the corner of the rectangle
+ * @param x the local X axis of the rectangle
+ * @param y the local Y axis of the rectangle
+ * @returns true iff 'p' lies inside of the rectangle; false otherwise
+ */
+bool inrect(vec3 p, vec3 c, vec3 x, vec3 y) {
+  vec2 xyp = vec2(dot(p-c, x), dot(p-c, y));
+  return all(greaterThanEqual(xyp, vec2(0.0)))
+      && all(lessThan(xyp, vec2(dot(x,x), dot(y,y))));
+}
+
+/**
+ * Compute the probability density value of randomRectangleAreaDirection()
+ * generating the vector 'v'.
+ *
+ * @param p the origin/point to sample from
+ * @param c the corner of the rectangle
+ * @param x the local X axis of the rectangle
+ * @param y the local Y axis of the rectangle
+ * @param v the vector to compute the pdf of
+ * @returns pdf(v) for the rectangle distribution
+ */
+float randomRectangleAreaDirectionPDF(vec3 p, vec3 c, vec3 x, vec3 y, vec3 v) {
+  vec3 n = cross(x, y);
+  float den = dot(n, v), t = dot(c - p, n) / den;
+  vec3 s = p + t * v, sp = p - s;
+  float len2 = dot(sp, sp);
+  vec3 d = sp * inversesqrt(len2);
+  return float(den < 0.0 && t > 0.0 && inrect(s, c, x, y)) * len2 / dot(d, n);
+}
---
+++ res/org/lwjgl/demo/opengl/raytracing/tutorial4_3/raytracing.glsl
@@ -0,0 +1,259 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: https://www.lwjgl.org/license
+ */
+#version 430 core
+
+#if GL_NV_gpu_shader5
+#extension GL_NV_gpu_shader5 : enable
+#elif GL_EXT_shader_8bit_storage
+#extension GL_EXT_shader_8bit_storage : enable
+#endif
+
+layout(binding = 0, rgba32f) uniform image2D framebufferImage;
+uniform vec3 eye, ray00, ray01, ray10, ray11;
+uniform float time;
+uniform int frameIndex;
+uniform float blendFactor;
+uniform bool useBlueNoise;
+uniform float phongExponent;
+uniform float specularFactor;
+
+layout(std430, binding = 0) readonly buffer SobolTex {
+  uint8_t[] sobols;
+};
+layout(std430, binding = 1) readonly buffer ScrambleTex {
+  uint8_t[] scrambles;
+};
+
+#define PI 3.14159265359
+#define TWO_PI 6.28318530718
+#define ONE_OVER_PI (1.0 / PI)
+#define ONE_OVER_2PI (1.0 / TWO_PI)
+#define LARGE_FLOAT 1E+10
+#define NUM_BOXES 11
+#define NUM_RECTANGLES 1
+#define EPSILON 1E-4
+#define BOUNCES 3
+#define LIGHT_INTENSITY 80.0
+#define PROBABILITY_OF_LIGHT_SAMPLE 0.6
+
+float random3(vec3 f);
+float random2(vec2 f);
+vec4 randomHemisphereDirection(vec3 n, vec2 rand);
+vec4 randomCosineWeightedHemisphereDirection(vec3 n, vec2 rand);
+float randomCosineWeightedHemisphereDirectionPDF(vec3 n, vec3 v);
+vec4 randomPhongWeightedHemisphereDirection(vec3 r, float a, vec2 rand);
+float randomPhongWeightedHemisphereDirectionPDF(vec3 r, float a, vec3 v);
+vec4 randomRectangleAreaDirection(vec3 p, vec3 c, vec3 x, vec3 y, vec2 rand);
+float randomRectangleAreaDirectionPDF(vec3 p, vec3 c, vec3 x, vec3 y, vec3 v);
+bool inrect(vec3 p, vec3 c, vec3 x, vec3 y);
+
+struct box {
+  vec3 min, max, col;
+};
+struct rectangle {
+  vec3 c;
+  vec3 x;
+  vec3 y;
+};
+const box boxes[NUM_BOXES] = {
+  {vec3(-5.0, -0.1, -5.0), vec3( 5.0, 0.0,  5.0), vec3(0.90, 0.85, 0.78)},// <- floor
+  {vec3(-5.1,  0.0, -5.0), vec3(-5.0, 5.0,  5.0), vec3(0.96, 0.96, 0.96)},   // <- left wall
+  {vec3( 5.0,  0.0, -5.0), vec3( 5.1, 5.0,  5.0), vec3(0.96, 0.96, 0.96)},   // <- right wall
+  {vec3(-5.0,  0.0, -5.1), vec3( 5.0, 5.0, -5.0), vec3(0.63, 0.82, 0.37)},// <- back wall
+  {vec3(-5.0,  0.0,  5.0), vec3( 5.0, 5.0,  5.1), vec3(0.96, 0.6, 0.29)},  // <- front wall
+  {vec3(-1.0,  1.0, -1.0), vec3( 1.0, 1.1,  1.0), vec3(0.3, 0.23, 0.15)*1.5}, // <- table top
+  {vec3(-1.0,  0.0, -1.0), vec3(-0.8, 1.0, -0.8), vec3(0.9, 0.6, 0.25)},  // <- table foot
+  {vec3(-1.0,  0.0,  0.8), vec3(-0.8, 1.0,  1.0), vec3(0.9, 0.6, 0.25)},  // <- table foot
+  {vec3( 0.8,  0.0, -1.0), vec3( 1.0, 1.0, -0.8), vec3(0.9, 0.6, 0.25)},  // <- table foot
+  {vec3( 0.8,  0.0,  0.8), vec3( 1.0, 1.0,  1.0), vec3(0.9, 0.6, 0.25)},  // <- table foot
+  {vec3( 3.0,  0.0, -4.9), vec3( 3.3, 2.0, -4.6), vec3(0.9, 0.8, 0.8)}    // <- some "pillar"
+};
+const rectangle rectangles[NUM_RECTANGLES] = {
+  {vec3(-3.0, 2.0, 3.0), vec3(6.0, 0.0, 0.0), vec3(0.0, -0.2, 0.05)}
+};
+
+struct hitinfo {
+  float near;
+  int i;
+  bool isRectangle;
+};
+
+ivec2 px;
+
+bool intersectRectangle(vec3 origin, vec3 dir, rectangle r, out hitinfo hinfo) {
+  vec3 n = cross(r.x, r.y);
+  float den = dot(n, dir), t = dot(r.c - origin, n) / den;
+  hinfo.near = t;
+  return den < 0.0 && t > 0.0 && inrect(origin + t * dir, r.c, r.x, r.y);
+}
+
+vec2 intersectBox(vec3 origin, vec3 dir, const box b) {
+  vec3 tMin = (b.min - origin) / dir;
+  vec3 tMax = (b.max - origin) / dir;
+  vec3 t1 = min(tMin, tMax);
+  vec3 t2 = max(tMin, tMax);
+  float tNear = max(max(t1.x, t1.y), t1.z);
+  float tFar = min(min(t2.x, t2.y), t2.z);
+  return vec2(tNear, tFar);
+}
+
+bool intersectObjects(vec3 origin, vec3 dir, out hitinfo info) {
+  float smallest = LARGE_FLOAT;
+  bool found = false;
+  /* Test the boxes */
+  for (int i = 0; i < NUM_BOXES; i++) {
+    vec2 lambda = intersectBox(origin, dir, boxes[i]);
+    if (lambda.y >= 0.0 && lambda.x < lambda.y && lambda.x < smallest) {
+      info.near = lambda.x;
+      info.i = i;
+      info.isRectangle = false;
+      smallest = lambda.x;
+      found = true;
+    }
+  }
+  /* Also test the rectangles */
+  for (int i = 0; i < NUM_RECTANGLES; i++) {
+    hitinfo hinfo;
+    if (intersectRectangle(origin, dir, rectangles[i], hinfo) && hinfo.near < smallest) {
+      info.near = hinfo.near;
+      info.i = i;
+      info.isRectangle = true;
+      smallest = hinfo.near;
+      found = true;
+    }
+  }
+  return found;
+}
+vec3 normalForRectangle(vec3 hit, const rectangle r) {
+  return cross(r.x, r.y);
+}
+vec3 normalForBox(vec3 hit, const box b) {
+  if (hit.x < b.min.x + EPSILON)
+    return vec3(-1.0, 0.0, 0.0);
+  else if (hit.x > b.max.x - EPSILON)
+    return vec3(1.0, 0.0, 0.0);
+  else if (hit.y < b.min.y + EPSILON)
+    return vec3(0.0, -1.0, 0.0);
+  else if (hit.y > b.max.y - EPSILON)
+    return vec3(0.0, 1.0, 0.0);
+  else if (hit.z < b.min.z + EPSILON)
+    return vec3(0.0, 0.0, -1.0);
+  else
+    return vec3(0.0, 0.0, 1.0);
+}
+
+float sampleBlueNoise(uint sampleIndex, uint sampleDimension) {
+  uint xoff = uint(random2(vec2(sampleIndex, frameIndex + px.x/128)) * 255.0);
+  uint yoff = uint(random2(vec2(sampleIndex+13, frameIndex + px.y/128)) * 255.0);
+  ivec2 pxo = (px + ivec2(xoff, yoff)) & 127;
+  sampleIndex = sampleIndex & 255;
+  sampleDimension = sampleDimension & 255;
+  uint value = sobols[sampleDimension + (sampleIndex << 8)];
+  value ^= scrambles[(sampleDimension & 7) + ((pxo.x + (pxo.y<<7))<<3)];
+  return (0.5 + float(value)) / 256.0;
+}
+
+vec3 randBlueNoise(int s) {
+  return vec3(
+    sampleBlueNoise(s, 0),
+    sampleBlueNoise(s, 1),
+    sampleBlueNoise(s, 2)
+  );
+}
+vec3 randWhiteNoise(int s) {
+  return vec3(
+    random3(vec3(px + ivec2(s), time)),
+    random3(vec3(px + ivec2(s), time*1.1)),
+    random3(vec3(px + ivec2(s), time*0.3)));
+}
+vec3 randvec3(int s) {
+  return useBlueNoise ? randBlueNoise(s) : randWhiteNoise(s);
+}
+
+vec3 brdfSpecular(vec3 i, vec3 o, vec3 n) {
+  float a = phongExponent;
+  vec3 r = reflect(-i, n);
+  return vec3(pow(max(0.0, dot(r, o)), a) * (a + 2.0) * ONE_OVER_2PI);
+}
+vec3 brdfDiffuse(vec3 albedo, vec3 i, vec3 o, vec3 n) {
+  return albedo * ONE_OVER_PI;
+}
+vec3 brdf(vec3 albedo, vec3 i, vec3 o, vec3 n) {
+  return brdfSpecular(i, o, n) * specularFactor
+         +
+         brdfDiffuse(albedo, i, o, n) * (1.0 - specularFactor);
+}
+
+vec3 trace(vec3 origin, vec3 dir) {
+  vec3 att = vec3(1.0);
+  for (int bounce = 0; bounce < BOUNCES; bounce++) {
+    hitinfo hinfo;
+    if (!intersectObjects(origin, dir, hinfo))
+       return vec3(0.0);
+    vec3 point = origin + hinfo.near * dir;
+    vec3 normal, albedo;
+    if (hinfo.isRectangle) {
+      const rectangle r = rectangles[hinfo.i];
+      return att * LIGHT_INTENSITY;
+    } else {
+      const box b = boxes[hinfo.i];
+      normal = normalForBox(point, b);
+      albedo = b.col;
+    }
+    origin = point + normal * EPSILON;
+    vec3 rand = randvec3(bounce);
+    rectangle li = rectangles[0];
+    vec4 s;
+    float wl = PROBABILITY_OF_LIGHT_SAMPLE;
+    vec3 ps = vec3(wl, specularFactor, 1.0 - specularFactor);
+    vec3 np = ps / (ps.x + ps.y + ps.z);
+    vec2 cdf = vec2(np.x, np.x + np.y);
+    vec3 p, c;
+    vec3 r = reflect(dir, normal);
+    if (rand.z < cdf.x) {
+      s = randomRectangleAreaDirection(origin, li.c, li.x, li.y, rand.xy);
+      p = vec3(s.w,
+               randomCosineWeightedHemisphereDirectionPDF(normal, s.xyz),
+               randomPhongWeightedHemisphereDirectionPDF(r, phongExponent, s.xyz));
+      c = np.xzy;
+    } else if (rand.z < cdf.y) {
+      s = randomPhongWeightedHemisphereDirection(r, phongExponent, rand.xy);
+      p = vec3(s.w,
+               randomCosineWeightedHemisphereDirectionPDF(normal, s.xyz),
+               randomRectangleAreaDirectionPDF(origin, li.c, li.x, li.y, s.xyz));
+      c = np.yzx;
+    } else {
+      s = randomCosineWeightedHemisphereDirection(normal, rand.xy);
+      p = vec3(s.w,
+               randomPhongWeightedHemisphereDirectionPDF(r, phongExponent, s.xyz),
+               randomRectangleAreaDirectionPDF(origin, li.c, li.x, li.y, s.xyz));
+      c = np.zyx;
+    }
+    s.w = dot(p, c);
+    att *= brdf(albedo, s.xyz, -dir, normal);
+    dir = s.xyz;
+    att *= max(0.0, dot(dir, normal));
+    if (s.w > 0.0)
+      att /= s.w;
+  }
+  return vec3(0.0);
+}
+
+layout (local_size_x = 16, local_size_y = 16) in;
+
+void main(void) {
+  px = ivec2(gl_GlobalInvocationID.xy);
+  ivec2 size = imageSize(framebufferImage);
+  if (any(greaterThanEqual(px, size)))
+    return;
+  vec2 p = (vec2(px) + vec2(0.5)) / vec2(size);
+  vec3 dir = mix(mix(ray00, ray01, p.y), mix(ray10, ray11, p.y), p.x);
+  vec3 color = trace(eye, normalize(dir));
+  vec3 oldColor = vec3(0.0);
+  if (blendFactor > 0.0)
+    oldColor = imageLoad(framebufferImage, px).rgb;
+  vec3 finalColor = mix(color, oldColor, blendFactor);
+  imageStore(framebufferImage, px, vec4(finalColor, 1.0));
+}
