f1b61d3bd66c725d51d18993e8f8fbaa5df72e73
httpdigest <kburjack@googlemail.com>
2021-09-10 14:17:12 +0200

    feat(Vulkan): add SimpleSphere demo using custom intersection shader

---
+++ res/org/lwjgl/demo/vulkan/raytracing/simplesphere/raymiss.glsl
@@ -0,0 +1,12 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: https://www.lwjgl.org/license
+ */
+#version 460
+#extension GL_EXT_ray_tracing : enable
+
+layout(location = 0) rayPayloadInEXT vec3 payload;
+
+void main(void) {
+  payload = vec3(0.0);
+}
