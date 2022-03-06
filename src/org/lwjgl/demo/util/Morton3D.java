12a4a23fb940e97790b9fea079f8ac41aa712a2f
httpdigest <kburjack@googlemail.com>
2022-03-06 17:18:37 +0100

    Add (5, 8, 5)-bits morton code encoding
    This will be useful when using z-order/Morton curves for spatial
    indexing of blocks within a (32, 256, 32)-blocks chunk using a hash
    table with hashes based on morton codes.

---
+++ src/org/lwjgl/demo/util/Morton3D.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: https://www.lwjgl.org/license
+ */
+package org.lwjgl.demo.util;
+
+/**
+ * Adapted from: http://graphics.stanford.edu/~seander/bithacks.html#InterleaveBMN
+ * 
+ * @author Kai Burjack
+ */
+public class Morton3D {
+  /**
+   * Encode the 5-bits X, 8-bits Y and 5-bits Z value into a 18-bits result with
+   * the Y value stored into the higher order bits.
+   *
+   * @param x the 5-bits x value
+   * @param y the 8-bits y value
+   * @param z the 5-bits z value
+   * @return the 18-bits morton code
+   */
+  public static int encode_y8_z5_x5(int x, int y, int z) {
+    return encode5(y) << 2 | (y & 0xE0) << 10 | encode5(z) << 1 | encode5(x);
+  }
+  private static int encode5(int v) {
+    v = (v | v << 8) & 0x100F;
+    v = (v | v << 4) & 0x10C3;
+    v = (v | v << 2) & 0x1249;
+    return v;
+  }
+}
